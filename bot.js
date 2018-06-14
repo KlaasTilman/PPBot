64933b78f28ba16c9e0cfbbda5074fc32526ce9b
HolmesXVIII <klaastilman@hotmail.com>
2018-06-14 20:32:41 +0200

    Refactoring

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
+const Response = require('./responseObjects.js');
+const Embeds = require('./embeds.js');
 const client = new Discord.Client();
 var isReady = true;
 
-function commandIs(str, msg){
+function commandSup(str, msg){
     return msg.content.toLowerCase().startsWith("sup " + str);
 }
 
@@ -21,197 +23,40 @@ function ttsBot(message, args) {
     }
 }
 
-function playVoiceCommand(message) {
+function playVoiceCommand(channel, voiceChannelID, voiceChannel, audioFile) {
     if (isReady) {
-        if (message.member.voiceChannelID==null) {
-            message.channel.sendMessage('You have to be in a voice chat to use this command.')
+        if (voiceChannelID==null) {
+            channel.sendMessage('You have to be in a voice chat to use this command.')
         } else if (isReady) {
             isReady=false;
-            var voiceChannel=message.member.voiceChannel;
             voiceChannel.join().then(connection =>{
-                const dispatcher=connection.playFile('./Voice files/'+voiceObject[message.content.toLowerCase()]);
+                const dispatcher=connection.playFile('./Voice files/'+audioFile);
                 dispatcher.on("end", end => {voiceChannel.leave();
                 isReady=true;});
             }).catch(err => console.log(err));
         } 
     } else {
-        message.channel.send("Please wait for the current voice command to end.");
+        channel.send("Please wait for the current voice command to end.");
     }
 }
 
-let voiceObject = {
-    "!pancake": 'pancake.mp3',
-    "!whoops": 'whoops.mp3',
-    "!magnumdong": 'magnumdong.mp3',
-    "!gotem": 'gotem.mp3',
-    "!niet": 'niet.mp3',
-    "!glitter": 'glitter.mp3',
-    "!darkness": 'darkness.mp3',
-    "!regret": 'sad.wav',
-    "regret": 'darkness.mp3',
-    "!shots": 'shots.mp3',
-    "!cant": 'cant.wav',
-    "!die": 'die.mp3',
-    "!freestyler": 'freestyler.wav',
-    "!wtf": 'wtf.mp3',
-    "!lidl": 'lidl.mp3',
-    "!badum": 'badum.mp3',
-    "!nexttime": 'nexttime.mp3',
-    "!no": 'no.wav',
-    "!never": 'never.wav',
-    "!lying": 'lying.wav',
-    "!wahahou": 'wahahou.wav',
-    "!chocola": 'chocola.wav',
-    "!profanity": 'profanity.wav',
-    "!tongue": 'tongue.wav',
-    "!alright": 'alright.wav',
-    "!goodbye": 'goodbye.wav',
-    "!komtgoed": 'komtgoed.wav',
-    "!poep": 'poep.wav',
-    "!bingo": 'bingo.wav',
-    "!myself": 'myself.wav',
-    "!houston": 'houston.wav',
-    "!try": 'try.wav',
-    "!tears": 'tears.wav',
-    "!endworld": 'endworld.wav',
-    "!missyou": 'missyou.wav',
-    "!together": 'together.wav',
-    "!steeds": 'steeds.wav',
-    "!hello": 'hello.wav',
-    "!tochniet": 'tochniet.wav',
-    "!skill": 'skill.wav',
-    "!dumb": 'dumb.wav',
-    "!biem": 'biem.wav',
-    "!fine": 'fine.mp3',
-    "!duuun": 'duuun.mp3',
-    "!happened": 'happened.mp3',
-    "!killmyself": 'killmyself.wav',
-    "!rustaagh": 'rustaagh.mp3',
-    "!dunked": 'dunked.wav',
-    "!infinity": 'infinity.mp3',
-    "!sad": 'sad.wav',
-    "!herewego": 'herewego.mp3',
-    "!mission": 'mission.mp3',
-    "!rip": 'rip.mp3',
-    "!trombone": 'trombone.mp3',
-    "!start": 'start.mp3',
-    "!noone": 'noone.mp3',
-    "!damnson": 'damnson.mp3',
-    "!tadaah": 'tadaah.mp3',
-    "!pressure": 'pressure.wav',
-    "!kapitein": 'kapitein.wav',
-    "!nigga": 'nigga.mp3',
-    "!like": 'like.mp3',
-    "!smooth": 'smooth.wav',
-    "!behappy": 'behappy.wav',
-    "!river": 'river.wav',
-    "!mylife": 'mylife.wav',
-    "!gratis": 'gratis.wav',
-    "!how": 'how.wav',
-    "!letsdoit": 'letsdoit.wav',
-    "!uhm": 'uhm.wav',
-    "!hehe": 'hehe.wav',
-    "!rutte": 'rutte.wav',
-    "!gohome": 'gohome.wav',
-    "!whatyoudoing": 'whatyoudoing.wav',
-    "!dontthinkso": 'dontthinkso.wav',
-    "!ruined": 'ruined.wav',
-    "!vibeit": 'vibeit.wav',
-    "!omgwow": 'omgwow.mp3',
-    "!sperdnigga": 'sperdnigga.mp3',
-    "!mbaku": 'mbaku.wav',
-    "!meneerdeboer": 'meneerdeboer.wav'
-};
-
-let responseObject = {
-    "ayy": "Ayy, lmao!",
-    "wat": "Say what?",
-    "lol": "roflmaotntpmp",
-    "!test": "Yes I am online :)",
-    "ping": "pong",
-    "pong": "ping",
-    "pief": "paf",
-    "paf": "pief",
-    "sup": "The Sky",
-    "oh shit waddup": "The Sky",
-    "!stats steam holmes": "!stats steam 76561198152188680",
-    "!niceshot": "https://gfycat.com/QuarrelsomeUnawareIslandwhistler",
-    "nice shot": "https://gfycat.com/QuarrelsomeUnawareIslandwhistler",
-    "🏓": "🏓",
-    "help": "Need help? (Yes PP/No PP)",
-    "!help": "Need help? (Yes PP/No PP)",
-    "no pp": "Okay! xx 🏓",
-    "yes pp": "Send `!commands` for a list of commands",
-    "regret": "Did you say regret!?",
-    "dab": "Did you say dab!?",
-    "!hackerman": "https://giphy.com/gifs/fury-kung-hackerman-QbumCX9HFFDQA"
-};
-
-let imageObject = {
-    "regret": './regret.png',
-    "!regret": './regret.png',
-    "dab": './dab.png',
-    "nigga": './nigga.png',
-    "really nigga": './nigga.png',
-    "!nigga": './nigga.png',
-    "meirl": './meirl2.png',
-    "meirll": './meirl.png',
-    "meirlll": './meirlmerge.jpg'
+function sendChatCommand(channel, chatMessage) {
+    channel.send(chatMessage);
 }
 
-let commandsEmbed = {embed: {
-        color: 3447003,
-        author: {
-            name: "",
-            icon_url: ""
-        },
-        description: "The awesome bot Ping Pong!",
-        title: "Voice Commands",
-        fields: [{
-            name: "Reaction 😯",
-            value: "niet, cant, die, badum, nexttime, no, lying, myself, houston, try, tears, shots, wtf, rustaagh, nigga, trombone, river, hehe, whatyoudoing, dontthinkso, ruined, omgwow, mbaku, meneerdeboer"
-        },
-        {
-            name: "Rocket League 🚙💥",
-            value: "kapitein, freestyler, dunked, smooth, herewego, start, skill, mission, tadaah, pressure, happened, duuun, gratis"
-        },
-        {
-            name: "Motivation 💪",
-            value: "together, komtgoed, infinity, never, behappy, mylife, letsdoit, vibeit"
-        },
-        {
-            name: "Sad 😭",
-            value: "fine, dumb, killmyself, sad, darkness, rip, missyou, goodbye, endworld, how, rutte, gohome"
-        },
-        {
-            name: "Meme",
-            value: "pancake, whoops, magnumdong, gotem, poep, glitter, lidl, wahahou, chocola, tongue, biem, like, hello, uhm, sperdnigga"
-        },
-        {
-            name: "Funny 😂",
-            value: "bingo, steeds, tochniet, alright, profanity, damnson, noone"
-        },
-        {
-            name: "Chat commands",
-            value: "ayy, wat, lol, ping, pong, pief, paf, sup, regret, help, dab, nigga, really nigga"
-        },
-        {
-            name: "Chat commands (!)",
-            value: "test, niceshot, help, regret, nigga, hackerman"
-        }
-        ],
-        footer: {
-            icon_url: "",
-            text: "© Klaas Tilman"
-        }
-    }
+function sendImageCommand(channel, image) {
+    channel.sendFile('./Image files/'+image);
 }
 
-function sendEmbed(message) {
-    commandsEmbed['embed']['author']['name']=client.user.username;
-    commandsEmbed['embed']['author']['icon_url']=client.user.avatarURL;
-    commandsEmbed['embed']['footer']['icon_url']=client.user.avatarURL;
-    message.channel.send(commandsEmbed);
+function initializeEmbedFields(embed) {
+    embed['embed']['author']['name']=client.user.username;
+    embed['embed']['author']['icon_url']=client.user.avatarURL;
+    embed['embed']['footer']['icon_url']=client.user.avatarURL;
+}
+
+function sendEmbed(channel, embed) {
+    initializeEmbedFields(embed);
+    channel.send(embed);
 }
 
 client.on('ready', () => {
@@ -221,22 +66,23 @@ client.on('ready', () => {
 client.on('message',message => {
     var args = message.content.split(/[ ]+/);
     ttsBot(message,args);
+    var messageLC=message.content.toLowerCase();
     // Text commands
-    if (responseObject[message.content.toLowerCase()] && message.author.username!='Ping Pong') {
-        message.channel.send(responseObject[message.content.toLowerCase()]);
+    if (Response.responseObject[messageLC] && message.author.username!='Ping Pong') {
+        sendChatCommand(message.channel, Response.responseObject[messageLC]);
     }
     // Image commands
-    if (imageObject[message.content.toLowerCase()]) {
-        message.channel.sendFile('./Image files/'+imageObject[message.content.toLowerCase()]);
+    if (Response.imageObject[messageLC]) {
+        sendImageCommand(message.channel, Response.imageObject[messageLC]);
     }
     // Voice commands
-    if (voiceObject[message.content.toLowerCase()]) {
-        playVoiceCommand(message);
+    if (Response.voiceObject[messageLC]) {
+        playVoiceCommand(message.channel, message.member.voiceChannelID, message.member.voiceChannel, Response.voiceObject[messageLC]);
     }
-    if(commandSay("commands",message)) {
-        sendEmbed(message);
+    if(Response.embeds[messageLC]) {
+        sendEmbed(message.channel, Response.embeds[messageLC]);
     }
-    if(message.content.toLowerCase().includes("ping pong")) {
+    if(messageLC.includes("ping pong")) {
         console.log("Bot was mentioned");
         message.channel.send("Say what?",{tts:true});
     }
---
+++ embeds.js
@@ -0,0 +1,129 @@
+exports.commands = {
+        embed: {
+        color: 3447003,
+        author: {
+            name: "",
+            icon_url: ""
+        },
+        description: "Send `!all` for all commands\n Send `!voicechat` for all voicechat commands \n Send `!chat` for all chat commands!",
+        title: "Documentation",
+        url: "https://github.com/KlaasTilman/DiscordPPBot",
+        footer: {
+            icon_url: "",
+            text: "© Klaas Tilman",
+        }
+    }
+}
+
+exports.allCommands = {embed: {
+        color: 3447003,
+        author: {
+            name: "",
+            icon_url: ""
+        },
+        description: "The awesome bot Ping Pong!",
+        fields: [{
+            name: "Reaction 😯",
+            value: "niet, cant, die, badum, nexttime, no, lying, myself, houston, try, tears, shots, wtf, rustaagh, nigga, trombone, river, hehe, whatyoudoing, dontthinkso, ruined, omgwow, mbaku, meneerdeboer"
+        },
+        {
+            name: "Rocket League 🚙💥",
+            value: "kapitein, freestyler, dunked, smooth, herewego, start, skill, mission, tadaah, pressure, happened, duuun, gratis"
+        },
+        {
+            name: "Motivation 💪",
+            value: "together, komtgoed, infinity, never, behappy, mylife, letsdoit, vibeit"
+        },
+        {
+            name: "Sad 😭",
+            value: "fine, dumb, killmyself, sad, darkness, rip, missyou, goodbye, endworld, how, rutte, gohome"
+        },
+        {
+            name: "Meme",
+            value: "pancake, whoops, magnumdong, gotem, poep, glitter, lidl, wahahou, chocola, tongue, biem, like, hello, uhm, sperdnigga"
+        },
+        {
+            name: "Funny 😂",
+            value: "bingo, steeds, tochniet, alright, profanity, damnson, noone"
+        },
+        {
+            name: "Chat commands",
+            value: "ayy, wat, lol, ping, pong, pief, paf, sup, regret, help, dab, nigga, really nigga"
+        },
+        {
+            name: "Chat commands (!)",
+            value: "test, niceshot, help, regret, nigga, hackerman"
+        }
+        ],
+        footer: {
+            icon_url: "",
+            text: "© Klaas Tilman",
+        }
+    }
+}
+
+exports.chatCommands = {embed: {
+        color: 3447003,
+        author: {
+            name: "",
+            icon_url: ""
+        },
+        title: "All chat commands",
+        fields: [{
+            name: "Simple response commands",
+            value: "ayy, wat, lol, ping, pong, pief, paf, sup, regret, help, dab, nigga, really nigga"
+        },
+        {
+            name: "Bot",
+            value: "test, help, commands, all, voicechat, chat"
+        },
+        {
+            name: "Images/Gifs",
+            value: "niceshot, regret, nigga, hackerman"
+        }
+        ],
+        footer: {
+            icon_url: "",
+            text: "© Klaas Tilman",
+        }
+    }
+}
+
+exports.voiceCommands = {embed: {
+        color: 3447003,
+        author: {
+            name: "",
+            icon_url: ""
+        },
+        title: "All voice commands",
+        fields: [{
+            name: "Reaction 😯",
+            value: "niet, cant, die, badum, nexttime, no, lying, myself, houston, try, tears, shots, wtf, rustaagh, nigga, trombone, river, hehe, whatyoudoing, dontthinkso, ruined, omgwow, mbaku, meneerdeboer"
+        },
+        {
+            name: "Rocket League 🚙💥",
+            value: "kapitein, freestyler, dunked, smooth, herewego, start, skill, mission, tadaah, pressure, happened, duuun, gratis"
+        },
+        {
+            name: "Motivation 💪",
+            value: "together, komtgoed, infinity, never, behappy, mylife, letsdoit, vibeit"
+        },
+        {
+            name: "Sad 😭",
+            value: "fine, dumb, killmyself, sad, darkness, rip, missyou, goodbye, endworld, how, rutte, gohome"
+        },
+        {
+            name: "Meme",
+            value: "pancake, whoops, magnumdong, gotem, poep, glitter, lidl, wahahou, chocola, tongue, biem, like, hello, uhm, sperdnigga"
+        },
+        {
+            name: "Funny 😂",
+            value: "bingo, steeds, tochniet, alright, profanity, damnson, noone"
+        }
+        ],
+        footer: {
+            icon_url: "",
+            text: "© Klaas Tilman",
+        }
+    }
+}
